a841d2afe143055d6a0f7c24b6fd510d41847c28
kghoreshi <kghoreshi@mitre.org>
2022-11-01 13:55:43 -0400

    add login page connected to keycloak

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './views/Login/Login';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './styles/theme'
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <Login />
+      </ThemeProvider>
     </div>
   );
 }
