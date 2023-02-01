a61f0462968aad14e82c614d4169cf3f4c22d4c9
Zach Robin <zrobin144@gmail.com>
2023-01-31 23:17:13 -0600

    added cors

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,17 +2,15 @@ const express = require('express')
 const doctorOrders = require("./routes/doctorOrders");
 const app = express()
 const port = process.env.PORT || 5051; 
-// const cors = require("cors");
+const cors = require("cors");
 
 //middleware and configurations 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-// app.use(cors());
+app.use(cors(['http://localhost:3000/', 'http://localhost:3008/']));
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-
 
 
 app.use('/doctorOrders',  doctorOrders);
