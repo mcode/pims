d510fa7c7ae9467db23e955c9621da05dce7ce60
Zach Robin <zrobin144@gmail.com>
2023-02-09 09:32:06 -0800

    linting changes

--- src/backend/routes/doctorOrders.js
+++ src/backend/routes/doctorOrders.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const doctorOrder = require('../database/schemas/doctorOrderSchemas.js')
+const doctorOrder = require('../database/schemas/doctorOrderSchemas.js');
 
 // XML Parsing Middleware used for NCPDP SCRIPT 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 require('body-parser-xml')(bodyParser);
 router.use(
     bodyParser.xml({
@@ -16,25 +15,24 @@ router.use(
 );
 router.use(bodyParser.urlencoded({ extended: false }));
 
-
 /**
- * Route: "doctorOrders/api/getRx"
- * Description : "Returns all documents in database for PIMS"
+ * Route: 'doctorOrders/api/getRx'
+ * Description : 'Returns all documents in database for PIMS'
  */
 router.get('/api/getRx', async(req, res) => {
     //  finding all and adding it to the db 
     const order = await doctorOrder.find(); 
 
-    console.log("Database return: ");
+    console.log('Database return: ');
     console.log(order);
     res.json(order);
 });
 
 /**
- * Route: "doctorOrders/api/addRx"
- * Description : "Saves a new Doctor Order to db"
+ * Route: 'doctorOrders/api/addRx'
+ * Description : 'Saves a new Doctor Order to db'
  */
-router.post("/api/addRx", async (req, res, next) => {
+router.post('/api/addRx', async (req, res) => {
     // Parsing incoming NCPDP SCRIPT XML to doctorOrder JSON
     const newOrder = parseNCPDPScript(req.body);
 
@@ -45,56 +43,40 @@ router.post("/api/addRx", async (req, res, next) => {
         return error;
     }
 
-    console.log("POST DoctorOrder: ");
+    console.log('POST DoctorOrder: ');
     console.log(newOrder);
     res.send(newOrder); 
-})
+});
 
 /**
  * Route : 'doctorOrders/api/getRx/:caseNumber`
- * Description : "Fetches doctor order bases on caseNumber"
+ * Description : 'Fetches doctor order bases on caseNumber'
  */
 // router.get('/api/getRx/:caseNumber', (req, res) => {
 //     const id = req.params.caseNumber;
 
-//     console.log("GET DoctorOrder: ");
+//     console.log('GET DoctorOrder: ');
 //     console.log(database);
 //     res.json(database);
 // });
 
 /**
- * Description : "Deletes all documents and prescriptions in PIMS"
+ * Description : 'Deletes all documents and prescriptions in PIMS'
  */
-
 router.delete('/api/deleteAll', async (req, res) => {
     await doctorOrder.deleteMany({});
-    console.log("All doctorOrders deleted in PIMS!");
-    res.send("DELETE Request Called");
+    console.log('All doctorOrders deleted in PIMS!');
+    res.send('DELETE Request Called');
 });
 
 
 /**
- * Description : "Returns parsed NCPDP NewRx as JSON"
+ * Description : 'Returns parsed NCPDP NewRx as JSON'
  * In : NCPDP SCRIPT XML <NewRx>
  * Return : Mongoose schema of a newOrder
  */
 function parseNCPDPScript(newRx) {
     // Parsing  XML NCPDP SCRIPT from EHR 
-    /*var newOrder = new doctorOrder({
-        caseNumber: newRx.Message.Header.MessageID.toString(), // Will need to return to this and use actual pt identifier or uuid
-        patientName: newRx.Message.Body.NewRx.Patient.HumanPatient.Names.Name.FirstName + ' ' + newRx.Message.Body.NewRx.Patient.HumanPatient.Names.Name.LastName,
-        patientDOB: newRx.Message.Body.NewRx.Patient.HumanPatient.DateOfBirth.Date,
-        doctorName: 'Dr. ' + newRx.Message.Body.NewRx.Prescriber.NonVeterinarian.Names.Name.FirstName + ' ' + newRx.Message.Body.NewRx.Prescriber.NonVeterinarian.Names.Name.LastName,
-        doctorContact: newRx.Message.Body.NewRx.Prescriber.NonVeterinarian.CommunicationNumbers.PrimaryTelephone.Number,
-        doctorID: newRx.Message.Body.NewRx.Prescriber.NonVeterinarian.Identification.NPI,
-        doctorEmail: newRx.Message.Body.NewRx.Prescriber.NonVeterinarian.CommunicationNumbers.ElectronicMail,
-        drugNames: newRx.Message.Body.NewRx.MedicationPrescribed.DrugDescription,
-        drugPrice: 200, // Add later?
-        quanitities: newRx.Message.Body.NewRx.MedicationPrescribed.Quantity.Value,
-        total: 1800,
-        pickupDate: 'Tue Dec 13 2022', // Add later?
-        dispenseStatus: 'Pending'
-    });*/
     var newOrder = new doctorOrder({
         caseNumber: newRx.Message.Header.MessageID.toString(), // Will need to return to this and use actual pt identifier or uuid
         patientName: newRx.Message.Body.NewRx.Patient.HumanPatient.Name.FirstName + ' ' + newRx.Message.Body.NewRx.Patient.HumanPatient.Name.LastName,
--- src/backend/server.js
+++ src/backend/server.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const doctorOrders = require("./routes/doctorOrders");
-const app = express()
+const doctorOrders = require('./routes/doctorOrders');
+const app = express();
 const port = process.env.PORT || 5051;
-const cors = require("cors");
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 //middleware and configurations 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 
 main().catch(err => console.log(err));
@@ -18,10 +18,10 @@ async function main() {
     app.listen(port, () => console.log(`Listening on port ${port}`));
     app.use('/doctorOrders', doctorOrders);
 
-    await mongoose.connect("mongodb://pims_remsadmin_mongo:27017/pims", {
-        authSource: "admin",
-        "user": "rems-admin-pims-root",
-        "pass": "rems-admin-pims-password",
+    await mongoose.connect('mongodb://pims_remsadmin_mongo:27017/pims', {
+        authSource: 'admin',
+        'user': 'rems-admin-pims-root',
+        'pass': 'rems-admin-pims-password',
     });
 }
 
