50495b2a4633609f61cb8a268b55d95cf73ca8cd
Sahil Malhotra <smalhotra@mitre.org>
2023-08-11 11:22:14 -0400

    dockerRunner updates

--- dockerRunnerDev.sh
+++ dockerRunnerDev.sh
@@ -4,12 +4,13 @@
 trap 'kill $CONTINUOUS_INSTALL_PID $SERVER_PID $BACKEND_SERVER_PID; exit' INT
 
 mkdir logs 
-# Reset log file content for new application boot
-echo "*** Logs for continuous frontend installer ***" > ./frontend/logs/installer.log
-echo "*** Logs for frontend 'npm run start' ***" > ./frontent/logs/runner.log
 
-echo "*** Logs for continuous backend installer ***" > ./backend/logs/installer.log
-echo "*** Logs for backend 'npm run start' ***" > ./backend/logs/runner.log
+# Reset log file content for new application boot
+echo "*** Logs for continuous frontend installer ***" > ./logs/frontend_installer.log
+echo "*** Logs for frontend 'npm run start' ***" > ./logs/frontend_runner.log
+
+echo "*** Logs for continuous backend installer ***" > ./logs/backend_installer.log
+echo "*** Logs for backend 'npm run start' ***" > ./logs/backend_runner.log
 
 # Print that the application is starting in watch mode
 echo "starting application in watch mode..."
@@ -18,9 +19,9 @@ echo "starting application in watch mode..."
 echo "starting continuous installers..."
 
 cd frontend
-npm install | tee ./frontend/logs/installer.log
+npm install | tee ./logs/frontend_installer.log
 cd ../backend
-npm install | tee ./backend/logs/installer.log
+npm install | tee ./logs/backend_installer.log
 cd ..
 
 ( package_modify_time=$(stat -c %Y frontend/package.json)
@@ -38,13 +39,13 @@ do
     then
         echo "running frontent npm install..."
         cd frontend
-        npm install | tee ./logs/installer.log
+        npm install | tee ./logs/frontend_installer.log
         cd ..
     elif [[ "$backend_lock_modify_time" != "$new_backend_lock_modify_time" ]]|| [[ "$backend_modify_time" != "$new_backend_modify_time" ]]
     then
         echo "running backend npm install..."
         cd backend
-        npm install | tee ./logs/installer.log
+        npm install | tee ./logs/backend_installer.log
         cd ..
     fi
 
@@ -57,10 +58,10 @@ done )  & CONTINUOUS_INSTALL_PID=$!
 
 # Start server process once initial build finishes  
 cd frontend
-( npm run start | tee ./frontent/logs/runner.log ) & SERVER_PID=$!
+( npm run start | tee ./logs/frontend_runner.log ) & SERVER_PID=$!
 
 cd ../backend
-( npm run start | tee ./backend/logs/runner.log ) & BACKEND_SERVER_PID=$!
+( npm run start | tee ./logs/backend_runner.log ) & BACKEND_SERVER_PID=$!
 
 # Handle application background process exiting
 wait $CONTINUOUS_INSTALL_PID $SERVER_PID $BACKEND_SERVER_PID
