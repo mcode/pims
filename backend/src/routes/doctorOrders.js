77c379dbc3bcdcffd43890c5ad2126eacd7f6d25
Ariel Virgulto <avirgulto@mitre.org>
2024-09-09 09:58:32 -0400

    Update to api call

--- backend/src/routes/doctorOrders.js
+++ backend/src/routes/doctorOrders.js
@@ -178,9 +178,9 @@ router.patch('/api/updateRx/:id/pickedUp', async (req, res) => {
     });
     console.log('Sent RxFill to EHR and received status from EHR', status.data);
 
-    const remsAdminStatus = await axios.post('http://rems-administrator:8090/ncpdp/script', rxFill, {
+    const remsAdminStatus = await axios.post(env.REMS_ADMIN_NCPDP, rxFill, {
       headers: {
-        Accept: 'application/xml', // Expect that the Status that the EHR returns back is in XML
+        Accept: 'application/xml', // Expect that the Status that the rems admin returns back is in XML
         'Content-Type': 'application/xml' // Tell the rems admin that the RxFill is in XML
       }
     });
