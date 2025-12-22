0848d8e242230d26260292d21d22c2cc5c3f080b
Sahil Malhotra <smalhotra@mitre.org>
2025-12-22 11:28:38 -0500

    undo xss middleware

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteXssMiddleware } from './vite-xss-middleware';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '.env' }); // load env vars from .env
 export default defineConfig({
   base: process.env.REACT_APP_VITE_BASE || '',
-  plugins: [react(), viteXssMiddleware()],
+  plugins: [react()],
   preview: {
     allowedHosts: ['.mitre.org', '.elb.us-east-1.amazonaws.com'],
     port: parseInt(process.env.PORT!),
